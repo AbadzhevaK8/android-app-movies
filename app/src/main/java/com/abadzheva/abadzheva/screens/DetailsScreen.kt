583d63098b18d318a52496a321033acd2bb6f894
k8abadzheva <make.me.loco@gmail.com>
2024-02-13 02:49:45 +0300

    feat: made a DetailsScreen view.

--- app/src/main/java/com/abadzheva/abadzheva/screens/DetailsScreen.kt
+++ app/src/main/java/com/abadzheva/abadzheva/screens/DetailsScreen.kt
@@ -1,11 +1,81 @@
 package com.abadzheva.abadzheva.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.navigation.NavHostController
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.rememberAsyncImagePainter
 import com.abadzheva.abadzheva.MainViewModel
+import com.abadzheva.abadzheva.utils.HtmlText
 
 @Composable
-fun DetailsScreen(navController: NavHostController, viewModel: MainViewModel, itemId: String) {
-    Text("Details screen: item id: ${itemId}")
+fun DetailsScreen(viewModel: MainViewModel, itemId: String) {
+
+    val currentItem = viewModel.allMovies
+        .observeAsState(listOf()).value
+        .firstOrNull { it.id == itemId.toInt() }
+
+    Surface(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(vertical = 50.dp, horizontal = 8.dp)
+    ) {
+        LazyColumn {
+            item {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Image(
+                        painter = rememberAsyncImagePainter(currentItem?.image?.medium),
+                        contentDescription = null,
+                        modifier = Modifier.size(512.dp)
+                    )
+                    Text(
+                        text = currentItem?.name ?: "",
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 32.sp,
+                        modifier = Modifier.padding(top = 16.dp)
+                    )
+                    Row(
+                        modifier = Modifier.padding(top = 8.dp)
+                    ) {
+                        Text(
+                            text = "Rating: ",
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 18.sp
+                        )
+                        Text(
+                            text = currentItem?.rating?.average.toString(),
+                            fontSize = 18.sp
+                        )
+                    }
+                    Row(
+                        modifier = Modifier.padding(top = 8.dp)
+                    ) {
+                        Text(
+                            text = "Genre: ",
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 18.sp
+                        )
+                        Text(
+                            text = currentItem?.genres?.take(2)?.joinToString(", ") ?: "",
+                        )
+                    }
+                    HtmlText(html = currentItem?.summary ?: "")
+                }
+            }
+        }
+    }
 }
--- app/src/main/java/com/abadzheva/abadzheva/utils/Constants.kt
+++ app/src/main/java/com/abadzheva/abadzheva/utils/Constants.kt
@@ -1,5 +1,12 @@
 package com.abadzheva.abadzheva.utils
 
+import android.text.Html
+import android.widget.TextView
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.text.HtmlCompat
+
 class Constants {
 
     object Screens {
@@ -7,4 +14,13 @@ class Constants {
         const val MAIN_SCREEN = "main_screen"
         const val DETAILS_SCREEN = "details_screen"
     }
+}
+
+@Composable
+fun HtmlText(html: String, modifier: Modifier = Modifier) {
+    AndroidView(
+        modifier = modifier,
+        factory = { context -> TextView(context) },
+        update = { it.text = HtmlCompat.fromHtml(html, HtmlCompat.FROM_HTML_MODE_COMPACT) }
+    )
 }
